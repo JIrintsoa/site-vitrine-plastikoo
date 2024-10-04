1487dea84849db757b0b2bccd8c7ae20663d72e2
JIrintsoa <johns.irintsoa@gmail.com>
2024-10-04 11:22:31 +0300

    avancement 04.10.24

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import pkg from '@next/env';
 
-export default nextConfig;
+const {loadEnvConfig} = pkg
+
+// import { loadEnvConfig } from '@next/env';
+
+// Load environment variables based on the environment (production or development)
+const isProd = process.env.NODE_ENV === 'production';
+
+// Load environment variables from .env files
+loadEnvConfig(process.cwd(), !isProd);
+
+export default {
+  // Conditional configurations based on the environment
+  output: isProd ? process.env.NEXT_PUBLIC_OUTPUT : undefined,
+  images: {
+    unoptimized: isProd ? process.env.NEXT_PUBLIC_IMAGES_UNOPTIMIZED === 'true' : false,
+  },
+  // Other common configurations can go here
+};
